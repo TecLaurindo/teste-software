9dbbbbde14d955396982eeae5616b23f3297af78
Vinicius.Laurindo <tecnicolaurindo@gmail.com>
2025-09-05 13:37:44 -0400

    add bliblioteca Scanner

--- src/main/java/rr/senac/Calculator.java
+++ src/main/java/rr/senac/Calculator.java
@@ -3,14 +3,25 @@
  */
 
 package rr.senac;
-
+import java.util.Scanner;
 /**
  *
  * @author devlaurindex
  */
 public class Calculator {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { //classe do tipo argument0
+        Scanner leia = new Scanner(System.in); //Comando para criar um scanner e ler as Teclas do Teclado
+        System.out.println("Digite os numeros a serem calculados:");
+        System.out.print("N°: ");
+        double num1 = leia.nextDouble();
+        System.out.println("N°: ");
+        double num2 = leia.nextDouble();
+
+        double soma;
+        
+        soma = num1 + num2;
+
+        System.out.println("O resultado do calculo é: "+ soma);
     }
 }
